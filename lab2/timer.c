e4f808914ae2e0c9fa142c77005fce74beac70cd
mafalda Santos <38763348+mafaldasantos10@users.noreply.github.com>
2018-10-08 21:13:20 +0000

    Changed the display function

--- lab2/timer.c
+++ lab2/timer.c
@@ -5,11 +5,13 @@
 
 #include "i8254.h"
 
-int (timer_set_frequency)(uint8_t UNUSED(timer), uint32_t UNUSED(freq)) {
-  /* To be completed by the students */
-  printf("%s is not yet implemented!\n", __func__);
+int (timer_set_frequency)(uint8_t (timer), uint32_t (freq)) 
+{
 
+ if (freq < 0)
   return 1;
+
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *UNUSED(bit_no)) {
@@ -49,19 +51,51 @@ int (timer_get_conf)(uint8_t (timer), uint8_t *(st))
   return 1;
  }
 
- sys_inb(TIMER_0 + timer, &status);
+if(timer == 0)
+{
+ sys_inb(TIMER_0, &status);
 
  //checks if the sys call was valid
- int res2 = sys_inb(TIMER_0 + timer, &status);
+ int res2 = sys_inb(TIMER_0, &status);
 
   if (res2 != OK)
  {
   printf ("Erro!");
   return 1;
  }
+}
+
+if (timer == 1)
+{
+sys_inb(TIMER_1, &status);
+
+ //checks if the sys call was valid
+ int res3 = sys_inb(TIMER_1, &status);
+
+  if (res3 != OK)
+ {
+  printf ("Erro!");
+  return 1;
+ }
+}
+
+if(timer == 2)
+{
+   sys_inb(TIMER_2, &status);
+
+ //checks if the sys call was valid
+ int res4 = sys_inb(TIMER_2, &status);
+
+  if (res4 != OK)
+ {
+  printf ("Erro!");
+  return 1;
+ }
+}
 
   return 0;
 }
+
 
 
 int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field)
@@ -69,10 +103,39 @@ int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field fiel
 
 union timer_status_field_val conf;
 
+if(field == base)
+{
+  // *******0
 conf.bcd = (st << 7) >> 7; 
+
+}
+
+if(field == initial)
+{
+  // ****3,2,1*
 conf.count_mode = (st << 4) >> 5;
+
+}
+
+if(field == mode)
+  // **5,4****
+{
 conf.in_mode = (st << 2) >> 6;
-conf.byte = st >> 6;
+
+}
+
+if(field == all)
+{
+  // 7,6,5,4,3,2,1,0
+conf.byte = st;
+
+}
+else
+{
+return 1;
+
+}
+
 
 timer_print_config(timer, field, conf);
 
