d14ca6441ac3adfba25b155f73ce4685ec337ab3
Diogo Alex <38763303+diogoalex01@users.noreply.github.com>
2018-10-09 21:47:13 +0000

    Mostly identation

--- lab2/timer.c
+++ lab2/timer.c
@@ -1,89 +1,89 @@
-#include <lcom/lcf.h>
-#include <lcom/timer.h>
+  #include <lcom/lcf.h>
+  #include <lcom/timer.h>
 
-#include <stdint.h>
- 
-#include "i8254.h"
+  #include <stdint.h>
 
-unsigned int counter =0;
+  #include "i8254.h"
+
+unsigned int counter = 0;
 
 int (timer_set_frequency)(uint8_t (timer), uint32_t (freq)) 
 {
 
-uint8_t st, lsb, msb;
-timer_get_conf(timer, &st);
+  uint8_t st, lsb, msb;
+  timer_get_conf(timer, &st);
 
-uint8_t control = TIMER_LSB_MSB | (st & 0x0F); //DATA ESTÁ PARADA, NÃO ATUALIZA!!!!
+  uint8_t control = TIMER_LSB_MSB | (st & 0x0F); //DATA ESTÁ PARADA, NÃO ATUALIZA!!!!
 
-switch(timer)
-{
+  switch(timer)
+  {
+    case 0:
+    {
+      control |= TIMER_SEL0;
+      break;
+    }
+    case 1:
+    {
+      control |= TIMER_SEL1;
+      break;
+    }
+    case 2:
+    {
+      control |= TIMER_SEL2;
+      break;
+    }
+    default:
+    {
+      return 1;
+    }
+  }
 
-case 0:
-{
-control |= TIMER_SEL0;
-break;
-}
-case 1:
-{
-control |= TIMER_SEL1;
-break;
-}
-case 2:
-{
-control |= TIMER_SEL2;
-break;
-}
-default:
-{
-  return 1;
-}
-}
+  
+  int res = sys_outb(TIMER_CTRL, control);
 
- //checks if the sys call was valid
- int res = sys_outb(TIMER_CTRL, control);
+  //checks if the sys call was valid
+  if (res != OK)
+  {
+    printf ("Erro!");
+    return 1;
+  }
 
- if (res != OK)
- {
-  printf ("Erro!");
-  return 1;
- }
+  uint16_t f_freq = TIMER_FREQ/freq;
 
-uint16_t f_freq = TIMER_FREQ/freq;
+  util_get_LSB(f_freq, &lsb);
 
-util_get_LSB(f_freq, &lsb);
+  util_get_MSB(f_freq, &msb);
 
-util_get_MSB(f_freq, &msb);
 
- //checks if the sys call was valid
- int res1 = sys_outb(TIMER_0 + timer, lsb);
+  int res1 = sys_outb(TIMER_0 + timer, lsb);
 
- if (res1 != OK)
- {
-  printf ("Erro!");
-  return 1;
- }
+  //checks if the sys call was valid
+  if (res1 != OK)
+  {
+    printf ("Erro!");
+    return 1;
+  }
 
- //checks if the sys call was valid
- int res2 = sys_outb(timer + TIMER_0, msb);
+  int res2 = sys_outb(timer + TIMER_0, msb);
 
- if (res2 != OK)
- {
-  printf ("Erro!");
-  return 1;
- }
+  //checks if the sys call was valid
+  if (res2 != OK)
+  {
+    printf ("Erro!");
+    return 1;
+  }
 
   return 0;
 
 }
 
-int (timer_subscribe_int)(uint8_t * UNUSED (bit_no)) {
-
-
+int (timer_subscribe_int)(uint8_t * UNUSED (bit_no)) 
+{
   return 1;
 }
 
 int (timer_unsubscribe_int)() {
-  /* To be completed by the students */
+    /* To be completed by the students */
   printf("%s is not yet implemented!\n", __func__);
 
   return 1;
@@ -97,100 +97,98 @@ void (timer_int_handler)()
 
 int (timer_get_conf)(uint8_t (timer), uint8_t *(st)) 
 {
- //
- uint8_t rb_command = TIMER_RB_CMD | TIMER_RB_SEL(timer);
+  uint8_t rb_command = TIMER_RB_CMD | TIMER_RB_SEL(timer);
 
- sys_outb(TIMER_CTRL, rb_command);
+  sys_outb(TIMER_CTRL, rb_command);
 
- uint32_t status = (uint32_t) *st;
- //checks if the sys call was valid
- int res = sys_outb(TIMER_CTRL, rb_command);
+  uint32_t status = (uint32_t) *st;
 
- if (res != OK)
- {
-  printf ("Erro!");
-  return 1;
- }
+  
+  int res = sys_outb(TIMER_CTRL, rb_command);
 
-if(timer == 0)
-{
- int res2 = sys_inb(TIMER_0, &status);
+  //checks if the sys call was valid
+  if (res != OK)
+  {
+    printf ("Erro!");
+    return 1;
+  }
 
-//checks if the sys call was valid
-  if (res2 != OK)
- {
-  printf ("Erro!");
-  return 1;
- }
-}
+  //selects the correct timer
+  if (timer == 0)
+  {
+    int res2 = sys_inb(TIMER_0, &status);
 
-if (timer == 1)
-{
-sys_inb(TIMER_1, &status);
+    //checks if the sys call was valid
+    if (res2 != OK)
+    {
+      printf ("Erro!");
+      return 1;
+    }
+  }
 
- //checks if the sys call was valid
- int res3 = sys_inb(TIMER_1, &status);
+  if (timer == 1)
+  {
+    int res3 = sys_inb(TIMER_1, &status);
 
-  if (res3 != OK)
- {
-  printf ("Erro!");
-  return 1;
- }
-}
+    //checks if the sys call was valid
+    if (res3 != OK)
+    {
+      printf ("Erro!");
+      return 1;
+    }
+  }
 
-if(timer == 2)
-{
-   sys_inb(TIMER_2, &status);
+  if (timer == 2)
+  {
+    int res4 = sys_inb(TIMER_2, &status);
 
- //checks if the sys call was valid
- int res4 = sys_inb(TIMER_2, &status);
-
-  if (res4 != OK)
- {
-  printf ("Erro!");
-  return 1;
- }
-}
+    //checks if the sys call was valid
+    if (res4 != OK)
+    {
+      printf ("Erro!");
+      return 1;
+    }
+  }
 
   return 0;
 }
-
 
 int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field)
 {
 
-union timer_status_field_val conf;
+  union timer_status_field_val conf;
 
-if(field == base)
-{
-// *******0
-conf.bcd = (st << 7) >> 7; 
-}
+  if (field == base)
+  {
+    // *******0
+    conf.bcd = (st << 7) >> 7; 
+  }
 
-if(field == initial)
-{
-// ****3,2,1*
-conf.count_mode = (st << 4) >> 5;
-}
+  if (field == initial)
+  {
+    // ****3,2,1*
+    conf.count_mode = (st << 4) >> 5;
+  }
 
-if(field == mode)
-// **5,4****
-{
-conf.in_mode = (st << 2) >> 6;
-}
+  if (field == mode)
+  {
+    // **5,4****
+    conf.in_mode = (st << 2) >> 6;
+  }
 
-if(field == all)
-{
-// 7,6,5,4,3,2,1,0
-conf.byte = st;
-}
+  if (field == all)
+  {
+    // 7,6,5,4,3,2,1,0
+    conf.byte = st;
+  }
 
-else
-{
-  return 1;
-}
+  else
+  {
+    return 1;
+  }
 
 timer_print_config(timer, field, conf);
 
-  return 0;
+return 0;
+
 }
