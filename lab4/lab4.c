9bc4f08c599fd54c2cc923d3daf2a9403e360f47
mafalda Santos <38763348+mafaldasantos10@users.noreply.github.com>
2018-11-06 19:11:29 +0000

    started async function

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,9 +1,13 @@
 // IMPORTANT: you must include the following line in all your C files
 #include <lcom/lcf.h>
+#include "PS2mouse.h"
+#include "i8042.h"
 
+#include <lcom/timer.h>
 #include <stdint.h>
 #include <stdio.h>
 
+uint32_t array[3];
 // Any header files included below this line should have been created by you
 
 int main(int argc, char *argv[]) {
@@ -36,6 +40,8 @@ int (mouse_test_packet)(uint32_t cnt) {
 	uint8_t irq_set;
 	int ipc_status, r, size = 1;
 	message msg;
+	
+	enable_int();
 
  	if (mouse_subscribe_int(&irq_set) != OK)
   	{
@@ -43,7 +49,7 @@ int (mouse_test_packet)(uint32_t cnt) {
   	}
   	// printf("%d", irq_set);
  
-  	while(1) 
+  	while(cnt > 0 ) 
   	{   /* You may want to use a different condition */ 
     	/* Get a request message. */ 
     	if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 )
@@ -59,7 +65,8 @@ int (mouse_test_packet)(uint32_t cnt) {
           			if (msg.m_notify.interrupts & BIT(irq_set)) 
           			{ /* subscribed interrupt */
 
-              			kbc_ih();
+              			mouse_ih();
+
             
             			//printf("status: %x", status);
 
@@ -68,20 +75,33 @@ int (mouse_test_packet)(uint32_t cnt) {
 
             			if(size == 1)
             			{
-              				size++;
+            				uint8_t temp = (uint8_t)status;
+            				if((temp<<4)>>7)
+            				{
+            				array[0] = status;
+            				size++;
+            			    }
+             				
               				continue;
             			} 
 
             			if(size == 2)
             			{
+            				array[1] = status;
               				size++;
               				continue;
             			}
-            		//falta guardar numa packet[]
-            		//falta a funcao para a imprimir, o stor n deu ainda
+
+            			array[2] = status;
+
+            			cnt--;
+            		packet_create();
+            		mouse_print_packet(&pp);
+
           			}
 
           			tickdelay(micros_to_ticks(DELAY_US));
+
 
           			size = 1;
           			break;
@@ -95,7 +115,9 @@ int (mouse_test_packet)(uint32_t cnt) {
     	}
   	}
 
-	if (kbd_unsubscribe_int() != OK)
+  	disable_int();
+
+	if (mouse_unsubscribe_int() != OK)
   	{
     	return 1;
   	}
@@ -104,18 +126,167 @@ int (mouse_test_packet)(uint32_t cnt) {
 }
 
 int (mouse_test_remote)(uint16_t period, uint8_t cnt) {
-    /* To be completed */
-    printf("%s(%u, %u): under construction\n", __func__, period, cnt);
-    return 1;
+
+//uint8_t irq_set;
+int size = 1;
+uint32_t array[3];
+
+
+    while(cnt > 0) 
+	{   
+		enable_poll();
+
+		if(mouse_poll() == -1)
+		{
+			continue;
+		}
+
+		if(size == 1)
+		{
+			uint8_t temp = (uint8_t)status;
+			if((temp<<4)>>7)
+			{
+				array[0] = status;
+				size++;
+			}
+
+			continue;
+		} 
+
+		if(size == 2)
+		{
+			array[1] = status;
+			size++;
+			continue;
+		}
+
+		array[2] = status;
+
+		cnt--;
+
+		packet_create();
+		mouse_print_packet(&pp);
+		tickdelay(micros_to_ticks(period));
+
+
+		size = 1;
+
+	}
+
+	disable_poll();
+	minix_get_dflt_kbc_cmd_byte();
+
+	return 0;
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, idle_time);
+
+  uint8_t irq_set_mouse, irq_set_timer;
+  int ipc_status, r, size = 1;
+  message msg;
+
+  enable_int();
+
+  if(idle_time<0)
+  {
+    return -1;
+  }
+
+  if (mouse_subscribe_int(&irq_set_mouse) != OK)
+  {
     return 1;
+  }
+  // printf("%d", irq_set);
+  if (timer_subscribe_int(&irq_set_timer) != OK)
+  {
+    return 1;
+  }
+ 
+  while(timer_counter < (idle_time * (uint8_t) sys_hz()) )
+  { 
+   /* Get a request message. */ 
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 )
+    {
+      printf("driver_receive failed with: %d", r); 
+      continue;
+    }
+    if (is_ipc_notify(ipc_status))
+    { /* received notification */ 
+      switch (_ENDPOINT_P(msg.m_source))
+      {
+        case HARDWARE: /* hardware interrupt notification */ 
+          if (msg.m_notify.interrupts & BIT(irq_set_timer)) 
+          {
+              timer_int_handler();
+          }
+          if (msg.m_notify.interrupts & BIT(irq_set_mouse)) 
+          { /* subscribed interrupt */ 
+
+           timer_counter = 0; //resets the timer if another interrupt is needed
+           
+       		 mouse_ih();
+
+       		 if(error == true)
+       		 	continue;
+
+       		 if(size == 1)
+       		 {
+       		 	uint8_t temp = (uint8_t)status;
+       		 	if((temp<<4)>>7)
+       		 	{
+       		 		array[0] = status;
+       		 		size++;
+       		 	}
+
+       		 	continue;
+       		 } 
+
+       		 if(size == 2)
+       		 {
+       		 	array[1] = status;
+       		 	size++;
+       		 	continue;
+       		 }
+
+       		 array[2] = status;
+
+       		 packet_create();
+       		 mouse_print_packet(&pp);
+
+          }
+
+          tickdelay(micros_to_ticks(DELAY_US));
+          size = 1;
+
+          break;
+        default:
+          break; /* no other notifications expected: do nothing */ 
+      } 
+    } 
+    else 
+    { /* received a standard message, not a notification */ 
+      /* no standard messages expected: do nothing */ 
+    }
+    
+  }
+
+  disable_int();
+  if (mouse_unsubscribe_int() != OK)
+  {  
+    return 1;
+  }
+
+  if (timer_unsubscribe_int() != OK)
+  {
+    return 1;
+  }
+
+
+  return 0;
 }
 
-int (mouse_test_gesture)() {
+
+int (mouse_test_gesture)(uint8_t UNUSED(x_len),uint8_t UNUSED(tolerance)) {
     /* To be completed */
     printf("%s: under construction\n", __func__);
     return 1;
