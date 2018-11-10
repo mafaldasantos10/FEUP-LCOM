8dc2854cf58e1f54e880bf01b733a9d1f4f76406
Diogo Alex <38763303+diogoalex01@users.noreply.github.com>
2018-11-10 19:42:48 +0000

    last improvements

--- lab4/PS2mouse.h
+++ lab4/PS2mouse.h
@@ -1,24 +1,14 @@
 #pragma once
 
 //global variables
-extern uint32_t status, stat; //global variable that contains the scan of the buffer
+extern uint32_t status; //global variable that contains the scan of the buffer
 extern uint32_t array[3];
 extern struct packet pp;
-extern struct packet saved_packet;
 extern bool error;
 extern int timer_counter;
 extern struct mouse_ev gest;
 typedef enum {INIT, DRAWL,HOLD, DRAWR, FINAL} state_t;
 extern state_t state;
-//extern uint32_t counter;
-//extern int timer_counter;
-//extern bool error;
-
-//#ifdef LAB4
-//int sys_inb_cnt(port_t port, uint32_t *byte);
-//#else
-//#define sys_inb_cnt(p,q) sys_inb(p,q) 
-//#endif
 
 //prototypes
 int mouse_subscribe_int(uint8_t *bit_no);
@@ -31,11 +21,13 @@ void packet_create();
 
 int mouse_write_int();
 
+bool check_call();
+
 int disable_int();
 
 int disable_poll();
 
-int mouse_poll_cmd(bool finish);
+int mouse_poll_cmd();
 
 int set_mouse(uint8_t write);
 
