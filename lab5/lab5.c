0fe5661391df4247a15553a7fbf3a819420ba2f3
up201706791 <up201706791@e468293f-f9b3-486e-af9f-a8ef22157b0a>
2018-11-13 11:17:59 +0000

    Imported lab5/

---
+++ lab5/lab5.c
@@ -0,0 +1,56 @@
+// IMPORTANT: you must include the following line in all your C files
+#include <lcom/lcf.h>
+
+#include <lcom/lab5.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+// Any header files included below this line should have been created by you
+
+int main(int argc, char *argv[]) {
+  // sets the language of LCF messages (can be either EN-US or PT-PT)
+  lcf_set_language("EN-US");
+
+  // enables to log function invocations that are being "wrapped" by LCF
+  // [comment this out if you don't want/need it]
+  lcf_trace_calls("/home/lcom/labs/lab5/trace.txt");
+
+  // enables to save the output of printf function calls on a file
+  // [comment this out if you don't want/need it]
+  lcf_log_output("/home/lcom/labs/lab5/output.txt");
+
+  // handles control over to LCF
+  // [LCF handles command line arguments and invokes the right function]
+  if (lcf_start(argc, argv))
+    return 1;
+
+  // LCF clean up tasks
+  // [must be the last statement before return]
+  lcf_cleanup();
+
+  return 0;
+}
+
+int (video_test_init)(uint16_t mode, uint8_t delay) {
+  /* To be completed */
+  printf("%s(0x%03x, %u): under construction\n", __func__, mode, delay);
+
+  return 1;
+}
+
+int (video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y,
+                       uint16_t width, uint16_t height, uint32_t color) {
+  /* To be completed */
+  printf("%s(0x%03X, %u, %u, %u, %u, 0x%08x): under construction\n",
+         __func__, mode, x, y, width, height, color);
+
+  return 1;
+}
+
+int (video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first) {
+  /* To be completed */
+  printf("%s(0x%03x, %u, 0x%08x): under construction\n", __func__, mode, no_rectangles, first);
+
+  return 1;
+}
