3fa97f0576a281d0e14e1171fd9026f7f7d0a989
up201706791 <up201706791@e468293f-f9b3-486e-af9f-a8ef22157b0a>
2018-11-20 13:07:34 +0000

    Added keyboard files

---
+++ lab5/keyboard.c
@@ -0,0 +1,116 @@
+#include <lcom/lcf.h>
+#include "keyboard.h"
+
+#include <stdint.h>
+#include <minix/sysutil.h> 
+
+#include "i8042.h"
+
+//global variables
+static int hook_id = 2;
+uint32_t status = 0, stat = 0, counter = 0;
+bool error2 = false;
+
+
+int (kbd_subscribe_int)(uint8_t *bit_no)
+{
+	*bit_no = hook_id;
+
+	//printf("dd %d\n", *bit_no );
+
+	//checks if the sys call was valid
+	if (sys_irqsetpolicy(IRQKEYBOARD, (IRQ_REENABLE | IRQ_EXCLUSIVE), &hook_id) != OK)
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+
+int (kbd_unsubscribe_int)() 
+{
+	//checks if the sys call was valid
+	if (sys_irqrmpolicy(&hook_id) != OK)
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+
+void (kbc_ih)(void) 
+{
+	int i = 0;
+	
+	while( i < 5 ) 
+	{
+		sys_inb(STAT_REG, &stat); /* assuming it returns OK */
+		/* loop while 8042 output buffer is empty */
+		if( stat & OBF ) 
+		{
+			sys_inb(OUT_BUF, &status); /* assuming it returns OK */
+
+			if ( (stat &(PAR_ERR | TO_ERR)) == 0 )
+			{
+				error2 = false;
+				return;
+			}
+			else
+			{
+				error2 = true;
+				return;
+			}
+		}
+
+		i++;
+	}
+
+	error2 = true;
+	return;
+}
+
+
+int kbd_poll_cmd()
+{
+	uint32_t kbdcmd;
+
+	sys_outb(KBC_CMD_REG, RD_CMD_B);
+	sys_inb(OUT_BUF, &kbdcmd);
+
+	kbdcmd = kbdcmd | KBC_EN;
+
+	sys_outb(KBC_CMD_REG, WRT_CMD_B);
+	sys_outb(OUT_BUF, kbdcmd);
+
+	return 0;
+}
+
+
+int kbd_scan_poll()
+{
+	int i = 0;
+	uint32_t statpoll = 0;
+
+	while( i < 5 )
+	{
+		sys_inb(STAT_REG, &statpoll); /* assuming it returns OK */
+		/* loop while 8042 output buffer is empty */
+		if( statpoll & OBF ) 
+		{
+			sys_inb(OUT_BUF, &status); /* assuming it returns OK */
+
+			if ( (stat & (PAR_ERR | TO_ERR | AUX) ) == 0 )
+				return (uint8_t) status;
+			else
+			{
+				return -1;
+			}
+		}
+
+		i++;
+	}
+
+	return -1;
+}
---
+++ lab5/keyboard.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//global variables
+extern uint32_t status; //global variable that contains the scan of the buffer
+extern uint32_t stat;
+extern uint32_t counter;
+extern int timer_counter;
+extern bool error2;
+
+
+
+//prototypes
+int kbd_subscribe_int(uint8_t *bit_no);
+
+int kbd_unsubscribe_int();
+
+uint8_t kbd_int_handler();
+
+int kbd_scan_poll();
+
+int kbd_poll_cmd();
+
+void kbd_poll();
