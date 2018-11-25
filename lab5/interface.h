5ea69e4c8fb3a4f0ca83236519c24df3658541e4
up201706892 <up201706892@e468293f-f9b3-486e-af9f-a8ef22157b0a>
2018-11-25 15:28:13 +0000

    get_mode_info completed

--- lab5/interface.h
+++ lab5/interface.h
@@ -7,8 +7,15 @@ extern int new_counter;
 //static int call();
 //static void * mapping();
 void pix_map(const char *xpm[], uint16_t x, uint16_t y);
+
 void draw_rectangle(uint16_t x, uint16_t y, uint16_t len, uint32_t color,uint16_t height);
+
 void draw_pattern(uint8_t no_rectangles, uint32_t first, uint8_t step, uint32_t mode);
+
 int pix_map_move_pos(const char *xpm[], uint16_t xf, uint16_t yf, int16_t speed, uint8_t fr_rate);
+
 int pix_map_move_neg(const char *xpm[], uint16_t xf, uint16_t yf, int16_t speed, uint8_t fr_rate);
+
 int Color_Mode(unsigned int i, unsigned int j, uint8_t no_rectangles, uint32_t first, uint8_t step, uint32_t mode);
+
+int get_mode_info(uint16_t mode, vbe_mode_info_t * vmi_p);
