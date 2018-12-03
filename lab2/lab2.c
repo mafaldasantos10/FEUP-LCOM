54b23b62f3a8aad639aca25d9eb008079de2b0b1
up201706892 <up201706892@e468293f-f9b3-486e-af9f-a8ef22157b0a>
2018-12-03 20:23:28 +0000

    timer possibly final commit

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -85,38 +85,38 @@ int(timer_test_int)(uint8_t time)
   }
  
   while(counter < (int)sys_hz() * time) 
-    {   /* You may want to use a different condition */ 
-        /* Get a request message. */ 
-      if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 )
-      { 
-        printf("driver_receive failed with: %d", r); 
-        continue; 
-      }
-      if (is_ipc_notify(ipc_status))
-      { /* received notification */ 
-        switch (_ENDPOINT_P(msg.m_source))
-        { 
-          case HARDWARE: /* hardware interrupt notification */ 
-            if (msg.m_notify.interrupts & BIT(irq_set)) 
-            { /* subscribed interrupt */ 
-              
-              timer_int_handler();
-
-              if(counter % (int)sys_hz() == 0)
-                {
-                  timer_print_elapsed_time();
-                }
-            } 
-            break; 
-          default: 
-            break; /* no other notifications expected: do nothing */ 
-        } 
-      } 
-      else 
-      { /* received a standard message, not a notification */ 
-        /* no standard messages expected: do nothing */ 
-      }
+  {   /* You may want to use a different condition */ 
+      /* Get a request message. */ 
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 )
+    { 
+      printf("driver_receive failed with: %d", r); 
+      continue; 
     }
+    if (is_ipc_notify(ipc_status))
+    { /* received notification */ 
+      switch (_ENDPOINT_P(msg.m_source))
+      { 
+        case HARDWARE: /* hardware interrupt notification */ 
+          if (msg.m_notify.interrupts & BIT(irq_set)) 
+          { /* subscribed interrupt */ 
+
+            timer_int_handler();
+
+            if(counter % (int)sys_hz() == 0)
+            {
+              timer_print_elapsed_time();
+            }
+          } 
+          break; 
+        default: 
+          break; /* no other notifications expected: do nothing */ 
+      } 
+    } 
+    else 
+    { /* received a standard message, not a notification */ 
+      /* no standard messages expected: do nothing */ 
+    }
+  }
 
   if (timer_unsubscribe_int() != OK)
   {
