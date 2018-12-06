f7299f721a139bb93afa167a1574d3136e933df6
mafalda Santos <38763348+mafaldasantos10@users.noreply.github.com>
2018-12-06 20:50:36 +0000

    solved transparency problem

--- proj/proj.c
+++ proj/proj.c
@@ -58,15 +58,15 @@ int (proj_main_loop)(int argc, char *argv[])
 
 	Bitmap * background = loadBitmap("/home/lcom/labs/proj/bitmap/discof.bmp"); 
   drawBitmap(background, 0, 0, ALIGN_LEFT);
-  Bitmap * pad = loadBitmap("/home/lcom/labs/proj/bitmap/pad5.bmp"); 
-  drawBitmap(pad, 430, 358, ALIGN_LEFT);
+  Bitmap * pad = loadBitmap("/home/lcom/labs/proj/bitmap/pad.bmp"); 
+  drawBitmap(pad, 430, 393, ALIGN_LEFT);
   Bitmap * arrowup = loadBitmap("/home/lcom/labs/proj/bitmap/arrowup.bmp"); 
   Bitmap * arrowright = loadBitmap("/home/lcom/labs/proj/bitmap/arrowright.bmp"); 
   Bitmap * arrowdown = loadBitmap("/home/lcom/labs/proj/bitmap/arrowdown.bmp"); 
   Bitmap * arrowleft = loadBitmap("/home/lcom/labs/proj/bitmap/arrowleft.bmp"); 
   Bitmap * cromossoma1 = loadBitmap("/home/lcom/labs/proj/bitmap/cromossoma1.bmp"); 
   Bitmap * cromossomaup = loadBitmap("/home/lcom/labs/proj/bitmap/cromossomaup.bmp"); 
-  drawBitmap(cromossoma1, 412, 50, ALIGN_LEFT);
+  drawBitmap(cromossoma1, 412, 20, ALIGN_LEFT);
   Bitmap * perfect = loadBitmap("/home/lcom/labs/proj/bitmap/perfect.bmp"); 
   Bitmap * great = loadBitmap("/home/lcom/labs/proj/bitmap/great.bmp"); 
   Bitmap * okay = loadBitmap("/home/lcom/labs/proj/bitmap/okay.bmp"); 
@@ -92,7 +92,7 @@ int (proj_main_loop)(int argc, char *argv[])
   bool esc = true, make = true, wait = false;
   message msg;
 
-  //drawBitmap(pad, 430, 358, ALIGN_LEFT);
+  //drawBitmap(pad, 430, 393, ALIGN_LEFT);
   //drawBitmap(arrow1, 430, 768, ALIGN_LEFT);
 
   while(esc)
@@ -183,29 +183,29 @@ int (proj_main_loop)(int argc, char *argv[])
         case 0:
         {
           right = true;
-          pix_map_move_pos(pad, background, arrowright, cromossoma1, 358, speed, fr_rate);
+          pix_map_move_pos(pad, background, arrowright, cromossoma1, 393, speed, fr_rate);
           break;
         }
          case 1:
         {
           up = true;
-          pix_map_move_pos(pad, background, arrowup, cromossoma1, 358, speed, fr_rate);
+          pix_map_move_pos(pad, background, arrowup, cromossoma1, 393, speed, fr_rate);
           break;
         }
          case 2:
         {
           down = true;
-          pix_map_move_pos(pad, background, arrowdown, cromossoma1, 358, speed, fr_rate);
+          pix_map_move_pos(pad, background, arrowdown, cromossoma1, 393, speed, fr_rate);
           break;
         }
          case 3:
         {
           left = true;
-          pix_map_move_pos(pad, background, arrowleft, cromossoma1, 358, speed, fr_rate);
+          pix_map_move_pos(pad, background, arrowleft, cromossoma1, 393, speed, fr_rate);
           break;
         }
       }
-      //pix_map_move_pos(pad, background, arrow, cromossoma1, 358, speed, fr_rate);
+      //pix_map_move_pos(pad, background, arrow, cromossoma1, 393, speed, fr_rate);
     }
   } 
 
@@ -223,6 +223,6 @@ int (proj_main_loop)(int argc, char *argv[])
   {
     return 1;
   }
-  
+ 
   return 0;
 }
