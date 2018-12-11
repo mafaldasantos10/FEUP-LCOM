473b7ab89c5ef69c3b09eeb929d6955d11c34b90
up201706791 <up201706791@e468293f-f9b3-486e-af9f-a8ef22157b0a>
2018-12-11 10:44:50 +0000

    moved game function to cromoparty.c

---
+++ proj/menu.c
@@ -0,0 +1,12 @@
+#include <machine/int86.h>
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+#include <minix/sysutil.h> 
+
+#include "i8042.h"
+#include "i8254.h"
+#include "cromoparty.h"
+#include "keyboard.h"
+#include "interface.h"
+#include "menu.h"
