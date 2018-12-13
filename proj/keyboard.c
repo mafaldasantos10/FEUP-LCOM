26715d98c676502123bf9e13bc82cf86bc57e9e0
up201706892 <up201706892@e468293f-f9b3-486e-af9f-a8ef22157b0a>
2018-12-13 17:40:26 +0000

    minor tweaks

--- proj/keyboard.c
+++ proj/keyboard.c
@@ -8,7 +8,7 @@
 
 //VARIABLE INITIALIZATION
 static int hook_id = 2;
-uint32_t status = 0, stat = 0, counter = 0;
+uint32_t status = 0;
 bool error = false;
 
 
@@ -47,7 +47,8 @@ int (kbd_unsubscribe_int)()
 void (kbc_ih)(void) 
 {
 	int i = 0;
-	
+	uint32_t stat;
+
 	while( i < 5 ) 
 	{
 		sys_inb(STAT_REG, &stat); /* assuming it returns OK */
@@ -73,49 +74,4 @@ void (kbc_ih)(void)
 
 	error = true;
 	return;
-}
-
-//////////////////////////////////////////////////////////////////
-
-int kbd_poll_cmd()
-{
-	uint32_t kbdcmd;
-
-	sys_outb(KBC_CMD_REG, RD_CMD_B);
-	sys_inb(OUT_BUF, &kbdcmd);
-
-	kbdcmd = kbdcmd | KBC_EN;
-
-	sys_outb(KBC_CMD_REG, WRT_CMD_B);
-	sys_outb(OUT_BUF, kbdcmd);
-
-	return 0;
-}
-
-//////////////////////////////////////////////////////////////////
-
-int kbd_scan_poll()
-{
-	int i = 0;
-
-	while( i < 5 )
-	{
-		sys_inb(STAT_REG, &stat); /* assuming it returns OK */
-		/* loop while 8042 output buffer is empty */
-		if( stat & OBF ) 
-		{
-			sys_inb(OUT_BUF, &status); /* assuming it returns OK */
-
-			if ( (stat & (PAR_ERR | TO_ERR | AUX) ) == 0 )
-				return (uint8_t) status;
-			else
-			{
-				return -1;
-			}
-		}
-
-		i++;
-	}
-
-	return -1;
 }
--- proj/menu.c
+++ proj/menu.c
@@ -12,6 +12,7 @@
 #include "menu.h"
 
 static state_t state = START;
+bool exit_game = false;
 
 int menu()
 {
@@ -31,80 +32,77 @@ int menu()
 
   uint8_t bit_no_kb;
 
-    if (kbd_subscribe_int(&bit_no_kb) != OK)
+  if (kbd_subscribe_int(&bit_no_kb) != OK)
+  {
+    return 1;
+  }
+
+  uint32_t irq_set_keyboard = BIT(bit_no_kb);
+
+  uint8_t byte1[1], byte2[2];
+  int ipc_status, r, size = 1;
+  bool wait = false;
+  message msg;
+
+  while (!exit_game) 
+  {
+    /* Get a request message. */
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0)
     {
-      return 1;
+      printf("driver_receive failed with: %d", r);
+      continue;
     }
-
-    uint32_t irq_set_keyboard = BIT(bit_no_kb);
-
-    uint8_t byte1[1], byte2[2];
-    int ipc_status, r, size = 1;
-    bool wait = false;
-    message msg;
-
-    while(1)
-    {  
-      /* Get a request message. */ 
-      if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 )
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source))
       {
-        printf("driver_receive failed with: %d", r); 
-        continue;
-      }
-      if (is_ipc_notify(ipc_status))
-      { /* received notification */ 
-        switch (_ENDPOINT_P(msg.m_source))
-        {
-          case HARDWARE: /* hardware interrupt notification */ 
-            if (msg.m_notify.interrupts & irq_set_keyboard) 
-            { /* subscribed interrupt */ 
+        case HARDWARE:  /* hardware interrupt notification */
+          if (msg.m_notify.interrupts & irq_set_keyboard)
+          { /* subscribed interrupt */
 
-              kbc_ih();
-            
-              if (status == MSB)
-              {
-                wait = true;
-                continue;
-              }
+            kbc_ih();
 
-              if (wait == true)
-              {
-                wait = false;
-                size = 2;
-              }
-
-              if (size == 1)
-              {
-                byte1[0] = status;
-              } 
-
-              if (size == 2)
-              {
-                byte2[0] = MSB;
-                byte2[1] = status;
-              }
-
-              change_state(bit_no_kb);
-              change_buttons();   
+            if (status == MSB)
+            {
+              wait = true;
+              continue;
             }
-            break;
-          default:
-            break; /* no other notifications expected: do nothing */ 
-          } 
-        } 
-        else
-        {  /* received a standard message, not a notification */ 
-          /* no standard messages expected: do nothing */ 
-        }
 
-      
-      size = 1;
-    } 
+            if (wait == true)
+            {
+              wait = false;
+              size = 2;
+            }
 
-    if (kbd_unsubscribe_int() != OK)
-    {  
-      return 1;
+            if (size == 1)
+            {
+              byte1[0] = status;
+            }
+
+            if (size == 2)
+            {
+              byte2[0] = MSB;
+              byte2[1] = status;
+            }
+
+            change_state(bit_no_kb);
+            change_buttons();
+          }
+          break;
+        default:
+          break; /* no other notifications expected: do nothing */
+      }
     }
+    else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
+
+    size = 1;
+  }
+
+  if (kbd_unsubscribe_int() != OK)
+  {
+    return 1;
+  }
 
   return 0;
 }
@@ -115,14 +113,13 @@ void change_state(uint8_t bit_no_kb)
   {
     case W_KEY:
       state = (state - 1) % 4;
-      printf("hello\n");
       break;
     case S_KEY:
       state = (state + 1) % 4;
       break;
     case ENTER_KEY:
       if (state == EXIT)
-        return;
+        exit_game = true;
       if (state == START)
         game(bit_no_kb);
       break;
@@ -134,15 +131,13 @@ void change_buttons()
   Bitmap * menu = loadBitmap("/home/lcom/labs/proj/bitmap/menu.bmp");
   drawBitmap(menu, 0, 0, ALIGN_LEFT);
   Bitmap * start_selected = loadBitmap("/home/lcom/labs/proj/bitmap/start2.bmp");
-  Bitmap * instructions_selected = loadBitmap("/home/lcom/labs/proj/bitmap/instructions2.bmp");
-  Bitmap * instructions_not_selected = loadBitmap("/home/lcom/labs/proj/bitmap/instructions1.bmp"); 
-  Bitmap * start_not_selected = loadBitmap("/home/lcom/labs/proj/bitmap/start1.bmp");
-
-  Bitmap * exit_not_selected = loadBitmap("/home/lcom/labs/proj/bitmap/exit1.bmp");  
-  Bitmap * highscore_not_selected = loadBitmap("/home/lcom/labs/proj/bitmap/highscores1.bmp"); 
-  Bitmap * exit_selected = loadBitmap("/home/lcom/labs/proj/bitmap/exit2.bmp");  
+  Bitmap * instructions_selected = loadBitmap("/home/lcom/labs/proj/bitmap/instructions2.bmp"); 
   Bitmap * highscore_selected = loadBitmap("/home/lcom/labs/proj/bitmap/highscores2.bmp");
-  
+  Bitmap * exit_selected = loadBitmap("/home/lcom/labs/proj/bitmap/exit2.bmp"); 
+
+  Bitmap * start_not_selected = loadBitmap("/home/lcom/labs/proj/bitmap/start1.bmp");
+  Bitmap * instructions_not_selected = loadBitmap("/home/lcom/labs/proj/bitmap/instructions1.bmp"); 
+  Bitmap * highscore_not_selected = loadBitmap("/home/lcom/labs/proj/bitmap/highscores1.bmp");  Bitmap * exit_not_selected = loadBitmap("/home/lcom/labs/proj/bitmap/exit1.bmp");
 
   printf("state -> %d\n", state);
 
@@ -166,14 +161,12 @@ void change_buttons()
       drawBitmap(exit_not_selected, 342, 665, ALIGN_LEFT);
       drawBitmap(instructions_not_selected, 342, 425, ALIGN_LEFT);
       drawBitmap(highscore_selected, 342, 545, ALIGN_LEFT);
-      //highscores
       break;
       case 3:
       drawBitmap(instructions_not_selected, 342, 425, ALIGN_LEFT);
       drawBitmap(start_not_selected, 342, 305, ALIGN_LEFT);
       drawBitmap(highscore_not_selected, 342, 545, ALIGN_LEFT);
       drawBitmap(exit_selected, 342, 665, ALIGN_LEFT);
-      //exit
       break;
   } 
 
