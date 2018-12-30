dc9a4ebc6a7a5f46cc237b2f6987f3686adfd9c4
mafalda Santos <38763348+mafaldasantos10@users.noreply.github.com>
2018-12-30 02:53:09 +0000

    rtc with date and new letters

--- proj/rtc.h
+++ proj/rtc.h
@@ -5,16 +5,25 @@
 #define MINUTE 0x02
 #define HOUR 0x04
 #define SECOND 0x00
+#define DAY 0x07
+#define MONTH 0x08
+#define YEAR 0x09
 #define BIT(n) (0x01<<(n))
 #define RTC_REG_A 0x0A
 #define RTC_REG_B 0x0B
 #define RTC_UIP BIT(7)
 #define BCD_MODE BIT(2)
 
-int wait_valid_rtc(uint32_t reg);
+int read_rtc(uint32_t reg);
 
 int get_hour();
 
 int get_min();
 
 int get_sec();
+
+int get_day();
+
+int get_month();
+
+int get_year();
